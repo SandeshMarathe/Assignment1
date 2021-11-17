1d049b9c33549fc256045532400c68086b6819d6
SandeshMarathe <marathesandesh1999@gmail.com>
2021-11-17 17:04:00 +0530

    Random Average Program

---
+++ randomAvg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+a=${RANDOM:0:2}
+b=${RANDOM:0:2}
+c=${RANDOM:0:2}
+d=${RANDOM:0:2}
+e=${RANDOM:0:2}
+
+sum=$(($a+$b+$c+$d+$e))
+
+echo "Sum of Five Random Number = $sum"
+
+avg=$((sum/5))
+
+echo "Average of Five Random Number = $avg"
+
